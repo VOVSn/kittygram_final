950a43e8ee26c888c2b2fa2c534a52379a2593e6
Vladimir Korolev <vovsnmail@gmail.com>
2025-01-22 11:02:13 +0300

    urls fix 1

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -9,11 +9,18 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+path_prefix = 'api/'
+
+api_patterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
+]
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path(path_prefix, include(api_patterns))
 ]
 
 if settings.DEBUG:
